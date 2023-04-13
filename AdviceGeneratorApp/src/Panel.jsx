173ca7cac9e9c37015e91ebf0a68c742b0aad031
jonathandly <dalyj138@gmail.com>
2023-04-13 15:58:29 -0400

    Implemented fetch request, to Advice API endpoint. Added text colors for Advice Id && Advice Body.

--- AdviceGeneratorApp/src/Panel.jsx
+++ AdviceGeneratorApp/src/Panel.jsx
@@ -1,17 +1,29 @@
 import PatternDividerDesktop from './assets/images/pattern-divider-desktop.svg'
 import IconDice from './assets/images/icon-dice.svg'
 import './Panel.css'
+import { useState } from 'react'
 
 function Panel() {
+    const [advice, setAdvice] = useState('')
+    const [adviceId, setAdviceId] = useState('')
+
+    async function fetchAdvice() {
+        let response = await fetch('https://api.adviceslip.com/advice')
+        let data = await response.json()
+        // console.log(data.slip.advice)
+        setAdviceId(data.slip.id)
+        setAdvice(data.slip.advice)
+    }
 
     return (
         <>
             <div className='panel'>
-                <p className='adviceNumber'>Advice</p>
-                <h3 className='adviceBody'>Body</h3>
+                <p className='adviceNumber'>ADVICE # {adviceId}</p>
+                <h3 className='adviceBody'>{advice}</h3>
+               
                 <img className='patternDividerDesktop' src={PatternDividerDesktop} />
             </div>
-            <div className='diceCircle'>
+            <div onClick={fetchAdvice} className='diceCircle'>
                 <img className='iconDice' src={IconDice} />
             </div>
         </>
